1ddb0ec5987008d76f738bc5d92e3b0531addfb6
Seanlin2 <seanlin22@gmail.com>
2020-05-08 10:02:58 +0800

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,57 @@
+var Main = new Vue({
+        el: "#main",
+        data: {
+          num:2,
+          nowDTlength:2,
+          DT: [{
+            Id:0,
+            text:'',
+          }]
+        },
+        created: function () {
+          this.init();
+        },
+        methods: {
+          init: function() {
+            var v = this;
+            for(i=0;i<v.num;i++){
+              this.DT[i] = this.NewItem(i+1,'one');
+            }            
+          },
+          Add: function(){
+            var v = this;
+            var nowlength = v.DT.length;
+            v.DT.push(v.NewItem( nowlength +1 ,'two'));
+            v.num = v.DT.length;
+          },          
+          Minus: function(){
+            var v = this;
+            v.DT.pop();
+            v.num = v.DT.length;
+          },
+          Change: function(){
+            var v = this;
+            var tDTL= v.DT.length;
+            var tnum= v.num;
+            
+            console.log('change :' + tDTL + ' to ' + tnum);
+            
+            if(tnum > tDTL){
+              for(i=0;i< (tnum - tDTL);i++){
+                v.Add();
+              }   
+            }else{
+              for(i=0;i< (tDTL - tnum);i++){
+                v.Minus();
+              }
+            }            
+          },
+          NewItem: function(Id,text){
+            var newitem = {
+              Id: Id,
+              text: text
+            };            
+            return newitem;
+          }
+        }
+      });
