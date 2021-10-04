87b9d4c5d0b112c623246bf1e25beade34c43dcc
Seanlin2 <seanlin22@gmail.com>
2021-10-04 10:29:58 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,14 @@ var Main = new Vue({
           this.Hideall()
           //ScrollOut
           ScrollOut({
-                /* options */
+            onShown: function(el) {
+            // use the web animation API
+              el.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+            },
+            onHidden: function(el) {
+              // hide the element initially
+              el.style.opacity = 0;
+            }
           });
         },
         methods: {
