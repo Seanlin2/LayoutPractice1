6f6d34521c39717240ebecb4379bb64310900aa6
Seanlin2 <seanlin22@gmail.com>
2021-10-04 17:44:19 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,19 +18,7 @@ var Main = new Vue({
         created: function () {
           this.Init();
           this.Hideall()
-          //ScrollOut
-          ScrollOut({
-            //targets: ".sticky-header",
-            //offset: 400,
-            onShown: function(el) {
-            // use the web animation API
-              el.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
-            },
-            onHidden: function(el) {
-              // hide the element initially
-              el.style.opacity = 0;
-            }
-          });
+         
         },
         methods: {
           Fun3:function(){
@@ -110,6 +98,20 @@ var getRow = function(rowIndex) {
     return arr;
 };
 
+ //ScrollOut
+//ScrollOut({
+//  //targets: ".sticky-header",
+//  //offset: 400,
+//  onShown: function(el) {
+//  // use the web animation API
+//    el.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+//  },
+//  onHidden: function(el) {
+//    // hide the element initially
+//    el.style.opacity = 0;
+//  }
+//});
+ScrollOut();
 function C(n,c){
     return NG(n,c)/NG(c,c);
 }
